b8b300da6f29f9db30f2d7b51d2faf4b273b9798
Max Petersen <mglxp5@gmail.com>
2018-09-14 19:47:12 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,6 +2,7 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     keys = []
-    puts "#{arguments.class} : #{arguments} | #{arguments.keys_of}"
+    #puts "#{arguments.class} : #{arguments} | #{arguments.keys_of}"
+    puts Hash.fetch(arguments)
   end
 end
